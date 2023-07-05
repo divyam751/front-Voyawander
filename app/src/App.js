0e0c38e6114acae0642332de40f97a931a426fde
unknown <hiteshsonawane900@gmail.com>
2023-07-05 11:53:07 +0530

    hotel

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import HomeContent from "./Components/HomContent";
-import Cards from "./Components/Cards";
-// import Card from "./Components/PlaceCard";
-import PlaceCard from "./Components/PlaceCard";
-import SignupCard from "./Components/SignupCard";
-import SigninCard from "./Components/SigninCard";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+
 
 // import { Card } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className='App'>
-      {/* <Navbar /> */}
-      <Home />
-      {/* <HomeContent /> */}
-      {/* <Cards /> */}
-      <PlaceCard />
-      <SignupCard />
-      <SigninCard />
-      <Contact />
-      <Footer />
+    <h1>Holiday</h1>
     </div>
   );
 }
